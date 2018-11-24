6be98e8df8e4d971bd01b31ae04c7426ec82a1e0
Sunil Singh A <sunil.singh.a@sap.com>
2018-11-24 20:27:16 +0530

    1st commit

---
+++ BackgroupVsThreads/BackgroupVsThreads/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace BackgroupVsThreads
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBackGroundworker = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStopworker = new System.Windows.Forms.Button();
+            this.BW = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnThread = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBackGroundworker
+            // 
+            this.btnBackGroundworker.Location = new System.Drawing.Point(472, 49);
+            this.btnBackGroundworker.Name = "btnBackGroundworker";
+            this.btnBackGroundworker.Size = new System.Drawing.Size(132, 57);
+            this.btnBackGroundworker.TabIndex = 0;
+            this.btnBackGroundworker.Text = "BackgroundWorker";
+            this.btnBackGroundworker.UseVisualStyleBackColor = true;
+            this.btnBackGroundworker.Click += new System.EventHandler(this.btnBackGroundworker_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 47);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Backgroundworkder Status:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStopworker
+            // 
+            this.btnStopworker.Location = new System.Drawing.Point(621, 49);
+            this.btnStopworker.Name = "btnStopworker";
+            this.btnStopworker.Size = new System.Drawing.Size(132, 57);
+            this.btnStopworker.TabIndex = 0;
+            this.btnStopworker.Text = "Stop Worker";
+            this.btnStopworker.UseVisualStyleBackColor = true;
+            this.btnStopworker.Click += new System.EventHandler(this.btnStopworker_Click);
+            // 
+            // BW
+            // 
+            this.BW.WorkerReportsProgress = true;
+            this.BW.WorkerSupportsCancellation = true;
+            this.BW.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BW_DoWork);
+            this.BW.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BW_ProgressChanged);
+            this.BW.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BW_RunWorkerCompleted);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(327, 47);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Thread Status:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(620, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Stop Thread";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnThread
+            // 
+            this.btnThread.Location = new System.Drawing.Point(471, 202);
+            this.btnThread.Name = "btnThread";
+            this.btnThread.Size = new System.Drawing.Size(132, 57);
+            this.btnThread.TabIndex = 3;
+            this.btnThread.Text = "Thread Worker";
+            this.btnThread.UseVisualStyleBackColor = true;
+            this.btnThread.Click += new System.EventHandler(this.btnThread_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 333);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnThread);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStopworker);
+            this.Controls.Add(this.btnBackGroundworker);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBackGroundworker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStopworker;
+        private System.ComponentModel.BackgroundWorker BW;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnThread;
+    }
+}
+
---
+++ BackgroupVsThreads/BackgroupVsThreads/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackgroupVsThreads
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region "BACKGROUND WORKDER"
+        private void btnBackGroundworker_Click(object sender, EventArgs e)
+        {
+            btnBackGroundworker.Enabled = false;
+            if (!BW.IsBusy)
+            {
+                BW.RunWorkerAsync();
+            }
+        }
+
+        private void BW_DoWork(object sender, DoWorkEventArgs e)
+        {
+            object myUpdate;
+            for (int i = 1; i < 1000; i++)
+            {
+                System.Threading.Thread.Sleep(10);
+                if (BW.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                if (i == 100)
+                {
+                    myUpdate = DateTime.Now;
+                    BW.ReportProgress(i, myUpdate);
+                }
+                if (i == 200)
+                {
+                    myUpdate = DateTime.Now;
+                    BW.ReportProgress(i, myUpdate);
+                }
+                if (i == 300)
+                {
+                    myUpdate = DateTime.Now;
+                    BW.ReportProgress(i, myUpdate);
+                }
+                if (i == 400)
+                {
+                    myUpdate = DateTime.Now;
+                    BW.ReportProgress(i, myUpdate);
+                }
+                if (i == 500)
+                {
+                    myUpdate = DateTime.Now;
+                    BW.ReportProgress(i, myUpdate);
+                }
+                if (i == 600)
+                {
+                    myUpdate = DateTime.Now;
+                    BW.ReportProgress(i, myUpdate);
+                }
+                if (i == 700)
+                {
+                    myUpdate = DateTime.Now;
+                    BW.ReportProgress(i, myUpdate);
+                }
+                if (i == 800)
+                {
+                    myUpdate = DateTime.Now;
+                    BW.ReportProgress(i, myUpdate);
+                }
+                if (i == 900)
+                {
+                    myUpdate = DateTime.Now;
+                    BW.ReportProgress(i, myUpdate);
+                }
+                if (i == 1000)
+                {
+                    myUpdate = DateTime.Now;
+                    BW.ReportProgress(i, myUpdate);
+
+                }
+            }
+        }
+
+        private void BW_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            label1.Text = e.ProgressPercentage + " - " + e.UserState.ToString();
+        }
+
+        private void BW_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                label1.Text = "USER CANCELLED!";
+            }
+            else
+            {
+                label1.Text = "Backgroundworker completed";
+            }
+            btnBackGroundworker.Enabled = true;
+        }
+
+        private void btnStopworker_Click(object sender, EventArgs e)
+        {
+            if (BW.IsBusy)
+            {
+                BW.CancelAsync();
+            }
+        }
+        #endregion
+
+        
+        #region "THREAD WORKER"
+        foo fooo;
+        private void btnThread_Click(object sender, EventArgs e)
+        {
+            this.btnThread.Enabled = false;
+            fooo = new foo();
+            fooo.cancelUpdate = false;
+            fooo.UpdateProgress += new EventHandler<ProgressArgs>(UpdateProgress);
+            fooo.Completed += new EventHandler(CompletedThread);
+            Thread t = new Thread(fooo.UpdateTime);
+            t.Start();
+        }
+
+
+        public delegate void paramDelegate(object sender, ProgressArgs e);
+
+        public void UpdateProgress(object sender, ProgressArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new paramDelegate(UpdateProgress), sender, e);
+            }
+            else
+            {
+                label2.Text = e.Status;
+            }
+        }
+
+        public delegate void BlankDelegate(object sender, EventArgs e);
+
+        public void CompletedThread(object sender, EventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new BlankDelegate(CompletedThread), sender, e);
+            }
+            else
+            {
+                btnThread.Enabled = true;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (fooo != null)
+            {
+                fooo.cancelUpdate = true;
+            }
+        }
+        #endregion
+
+    }
+}
+
+public class foo
+{
+    public event EventHandler<ProgressArgs> UpdateProgress;
+
+    public event EventHandler Completed;
+
+    public bool cancelUpdate;
+
+    public void UpdateTime()
+    {
+        string myUpdate = string.Empty;
+        for (int i = 1; i < 1000; i++)
+        {
+
+            if (cancelUpdate)
+            {
+                myUpdate = "USER CANCELLED";
+                if (UpdateProgress != null)
+                {
+                    UpdateProgress(this, new ProgressArgs(0, myUpdate));
+
+                    if (Completed != null)
+                    {
+                        Completed(this, null);
+                    }
+
+                }
+                return;
+            }
+
+            System.Threading.Thread.Sleep(10);
+
+          
+            if (i == 100)
+            {
+                myUpdate = i + " - " + DateTime.Now;      
+                
+            }
+            if (i == 200)
+            {
+                myUpdate = i + " - " + DateTime.Now;
+            }
+            if (i == 300)
+            {
+                myUpdate = i + " - " + DateTime.Now;
+            }
+            if (i == 400)
+            {
+                myUpdate = i + " - " + DateTime.Now;
+            }
+            if (i == 500)
+            {
+                myUpdate = i + " - " + DateTime.Now;
+            }
+            if (i == 600)
+            {
+                myUpdate = i + " - " + DateTime.Now;
+            }
+            if (i == 700)
+            {
+                myUpdate = i + " - " + DateTime.Now;
+            }
+            if (i == 800)
+            {
+                myUpdate = i + " - " + DateTime.Now;
+            }
+            if (i == 900)
+            {
+                myUpdate = i + " - " + DateTime.Now;
+            }
+            if (i == 1000)
+            {
+                myUpdate = i + " - " + DateTime.Now;
+
+            }
+
+            if (UpdateProgress != null)
+            {               
+                UpdateProgress(this, new ProgressArgs(i, myUpdate));
+
+            }
+
+          
+        }
+
+        if (UpdateProgress != null)
+        {
+            UpdateProgress(this, new ProgressArgs(1000, "THREAD COMPLETED"));
+
+        }
+
+        if (Completed != null)
+        {
+            Completed(this, null);
+        }
+
+    }
+
+    
+
+}
+
+public class ProgressArgs : EventArgs
+{
+    int _PercentageCompleted;
+    string _status;
+
+    public ProgressArgs(int PercentageCompleted, string status)
+    {
+        _PercentageCompleted = PercentageCompleted;
+        _status = status;
+    }
+
+    public int PercentageCompleted { get { return _PercentageCompleted; } }
+    public string Status { get { return _status; } }
+
+}
